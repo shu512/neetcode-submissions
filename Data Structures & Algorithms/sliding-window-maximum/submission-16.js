b28662f84416e20cbe13c3ec1d004e6c52b24655
Stanislav Gusev <79231049479@yandex.ru>
2026-04-13 12:02:23 +0300

    Add: sliding-window-maximum - submission-16

---
+++ Data Structures & Algorithms/sliding-window-maximum/submission-16.js
@@ -0,0 +1,37 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} k
+     * @return {number[]}
+     */
+    maxSlidingWindow(nums, k) {
+        const deq = [];
+        let r = 0;
+        const res = [];
+
+        while(r < k - 1) {
+            this.add(deq, nums[r]);
+            r++;
+        }
+        this.add(deq, nums[r]);
+
+        while(r < nums.length) {
+            const l = r - k + 1;
+            const toPush = nums[l] === deq[0] ? deq.shift() : deq[0];
+            res.push(toPush);
+            r++;
+
+            this.add(deq, nums[r]);
+        }
+
+        return res;
+    }
+
+    add(deq, num) {
+
+        while(deq[deq.length - 1] < num) {
+            deq.pop();
+        }
+        deq.push(num);
+    }
+}
