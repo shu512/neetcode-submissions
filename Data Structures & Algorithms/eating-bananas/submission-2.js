6f9028f7721936383d437ab38bc77ac50b9c1fcc
Stanislav Gusev <79231049479@yandex.ru>
2026-04-20 10:47:51 +0300

    Add: eating-bananas - submission-2

---
+++ Data Structures & Algorithms/eating-bananas/submission-2.js
@@ -0,0 +1,28 @@
+class Solution {
+    /**
+     * @param {number[]} piles
+     * @param {number} h
+     * @return {number}
+     */
+    minEatingSpeed(piles, h) {
+        let minK = 9999999999;
+        let l = 1, r = 1000000000;
+
+        while(l <= r) {
+            const middle = l + Math.floor((r - l) / 2);
+            if (this.ok(piles, h, middle)) {
+                minK = middle;
+                r = middle - 1;
+            } else {
+                l = middle + 1;
+            }
+        }
+
+        return minK;
+    }
+
+    ok(piles, h, k) {
+        let sumH = piles.reduce((sum, p) => sum + Math.ceil(p / k), 0);
+        return sumH <= h;
+    }
+}
