8f54eaf05514c9f3dad1120778c6d0414aef9ddf
Stanislav Gusev <79231049479@yandex.ru>
2026-04-27 13:26:20 +0300

    Add: time-based-key-value-store - submission-0

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-0.js
@@ -0,0 +1,53 @@
+class TimeMap {
+    constructor() {
+        this.keyStore = new Map();
+    }
+
+    /**
+     * @param {string} key
+     * @param {string} value
+     * @param {number} timestamp
+     * @return {void}
+     */
+    set(key, value, timestamp) {
+        if (!this.keyStore.get(key)) {
+            this.keyStore.set(key, []);
+        }
+        const obj = {
+            timestamp,
+            value,
+        };
+        this.keyStore.get(key).push(obj);
+    }
+
+    /**
+     * @param {string} key
+     * @param {number} timestamp
+     * @return {string}
+     */
+    get(key, timestamp) {
+        const arr = this.keyStore.get(key)
+        if (!arr) return '';
+        return this._bSearch(arr, timestamp);
+    }
+
+    _bSearch(arr, timestamp) {
+        let mostValue = '';
+        let l = 0, r = arr.length - 1;
+        while(l <= r) {
+            const mid = l + Math.floor((r - l) / 2);
+            const cur = arr[mid];
+            if (cur.timestamp === timestamp) {
+                return cur.value;
+            }
+            if (cur.timestamp < timestamp) {
+                l = mid + 1;
+                mostValue = cur.value;
+            } else {
+                r = mid - 1;
+            }
+        }
+
+        return mostValue;
+    }
+}
