32249d8236a9946b7ca25794a36e6102b05546c5
Stanislav Gusev <79231049479@yandex.ru>
2026-06-22 12:11:47 +0700

    Add: design-word-search-data-structure - submission-0

---
+++ Data Structures & Algorithms/design-word-search-data-structure/submission-0.ts
@@ -0,0 +1,55 @@
+class TrieNode {
+    children: TrieNode[];
+    letter: string;
+    end: boolean;
+
+    constructor(letter: string, end = false) {
+        this.children = [];
+        this.letter = letter;
+        this.end = end;
+    }
+}
+
+class WordDictionary {
+    root: TrieNode;
+    constructor() {
+        this.root = new TrieNode('');
+    }
+
+    /**
+     * @param {string} word
+     * @return {void}
+     */
+    addWord(word: string): void {
+        let cur = this.root;
+        for (let char of word) {
+            if (!cur.children[char]) cur.children[char] = new TrieNode(char);
+            cur = cur.children[char];
+        }
+        cur.end = true;
+    }
+
+    /**
+     * @param {string} word
+     * @return {boolean}
+     */
+    search(word: string, start = this.root): boolean {
+        let cur = start;
+        console.log('started, word = ', word)
+        for (let i = 0; i < word.length; i++) {
+            let char = word[i];
+            if (char == '.') {
+                let res = false;
+                for (let key in cur.children) {
+                    if (this.search(word.slice(i+1), cur.children[key])) res = true;
+                }
+                return res;
+            }
+
+            if (!cur.children[char]) return false;
+            cur = cur.children[char];
+        }
+
+        return cur.end;
+    }
+}
